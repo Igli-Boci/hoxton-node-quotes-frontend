fbc0fbd0106de15d62d131d8e177e0e09b7b9ea0
Igli-Boci <igliboci10@gmai.com>
2022-08-31 05:06:04 +0200

    Fetching quotes from the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Quote } from "./types";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3010/quotes")
+      .then((resp) => resp.json())
+      .then((quotesFromServer) => setQuotes(quotesFromServer));
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <ul>
+      {quotes.map((quote) => (
+        <li className="quotes">
+          <p>{quote.quote}</p>
+          <h3>{quote.author}</h3>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default App
+export default App;
